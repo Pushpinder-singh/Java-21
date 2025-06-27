dc082b05796bc164a5170aa9c5fd22e7794ee862
pushpinder singh <pushpinder.1990singh@gmail.com>
2025-06-27 10:38:39 -0400

    updated code

--- JavaExamples/src/ArrayListMutable.java
+++ JavaExamples/src/ArrayListMutable.java
@@ -6,10 +6,16 @@ import java.util.List;
 public class ArrayListMutable {
 
     public static void main(String[] args){
+        
+        System.out.println("Difference between Arrays.asList() and List.Of() Array List");
         System.out.println("Muttable behavior of Array List");
 
+
+        /* if add() is used will get exception , if set() is used elements can be modified*/
         List<Integer> myList  = Arrays.asList(1,2,3,4);
 
+
+        /* Only Immutable behavior*/
         List<Integer> myList2 = List.of(4,5,6,89);
         try {
             //myList.add(5);
