e8712798aff564d878ee2e3ed3cdd97c3a5e8e22
pushpinder singh <pushpinder.1990singh@gmail.com>
2026-05-25 17:42:57 -0400

    Java 17 features logic

--- JavaExamples/src/record_Patterns/patternMatchingSwitchTest.java
+++ JavaExamples/src/record_Patterns/patternMatchingSwitchTest.java
@@ -1,18 +1,20 @@
 package record_Patterns;
 
 public class patternMatchingSwitchTest {
-	
-	public static void main(String[] args) {
-	
-		//	Object obj = "Hello, Java!";
-		
-		Object obj = 43;
-		
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        // Object obj = "Hello, Java!";
+
+        Object obj = 43;
+
         switch (obj) {
             case String s -> System.out.println("It's a string: " + s);
             case Integer i -> System.out.println("It's an integer: " + i);
             default -> System.out.println("Unknown type");
         }
-	}
+    }
 
 }
