d84adc41322cb04db93289ecd2ca1e2a5c4b15fe
pushpinder singh <pushpinder.1990singh@gmail.com>
2026-05-27 00:15:36 -0400

    String operations

---
+++ JavaExamples/src/String/isEmptyisBlank.java
@@ -0,0 +1,14 @@
+package String;
+
+public class isEmptyisBlank {
+    public static void main(String[] args) {
+        System.out.println("".isEmpty());
+        System.out.println(" ".isEmpty());
+        System.out.println("\n".isEmpty());
+
+System.out.println();
+        System.out.println("".isBlank());
+        System.out.println(" ".isBlank());
+        System.out.println("\n".isBlank());
+    }
+}
---
+++ JavaExamples/src/stringTemplates/StringLiteralTest.java
@@ -0,0 +1,20 @@
+package stringTemplates;
+
+import java.lang.*;
+
+public class StringLiteralTest {
+
+	public static void main(String[] args) {
+
+		String name = "Innovations world";
+		// String welcomeText = STR.`Welcome to \{name}`;
+		System.out.println(name);
+/*
+		String name = "Pushpinder Singh";
+		String country = "Canada";
+		String template = STR."Hello \\{name} welcome to \\{Country} office.";
+				System.out.println(template);
+*/
+	}
+
+}
